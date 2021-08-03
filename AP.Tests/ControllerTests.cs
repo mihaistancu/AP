18742e5035ca49f1a5f35b3ed977fb67834eab42
ubuntucsharp <ubuntucsharp@gmail.com>
2021-08-03 13:35:17 +0200

    Add controller

---
+++ AP.Tests/ControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using AP.Receiver;
+using AP.Tests.TestDoubles;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AP.Tests
+{
+    [TestClass]
+    public class ControllerTests
+    {
+        [TestMethod]
+        public void CallsPipeline()
+        {
+            var pipeline = new Pipeline();
+
+            var handler = new HandlerSpy();
+            pipeline.Add(handler);
+            var controller = new Controller(pipeline);
+
+            var message = new Message();
+            controller.Handle(message);
+
+            Assert.IsTrue(handler.WasCalled);
+        }
+    }
+}
---
+++ AP/Controller.cs
@@ -0,0 +1,19 @@
+﻿using AP.Receiver;
+
+namespace AP
+{
+    public class Controller
+    {
+        private Pipeline pipeline;
+
+        public Controller(Pipeline pipeline)
+        {
+            this.pipeline = pipeline;
+        }
+
+        public void Handle(Message message)
+        {
+            pipeline.Process(message);
+        }
+    }
+}
