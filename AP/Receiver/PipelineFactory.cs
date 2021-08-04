8c6d292ed78d7a3658204b2da7ba18c8cc645eb2
ubuntucsharp <ubuntucsharp@gmail.com>
2021-08-04 12:11:42 +0200

    Refactoring

---
+++ AP/Receiver/PipelineFactory.cs
@@ -0,0 +1,40 @@
+﻿using AP.Receiver.Handlers;
+
+namespace AP.Receiver
+{
+    public class PipelineFactory
+    {
+        private Pipeline businessInbound;
+        private Pipeline nonBusinessInbound;
+
+        public PipelineFactory()
+        {
+            var tlsCheck = new TlsCheckHandler();
+            var decryption = new DecryptionHandler();
+            var signatureCheck = new SignatureCheckHandler();
+            var validation = new ValidationHandler();
+            var persistence = new PersistenceHandler();
+            
+            businessInbound = new Pipeline();
+            businessInbound.Add(tlsCheck);
+            businessInbound.Add(decryption);
+            businessInbound.Add(validation);
+            businessInbound.Add(persistence);
+
+            nonBusinessInbound = new Pipeline();
+            nonBusinessInbound.Add(tlsCheck);
+            nonBusinessInbound.Add(signatureCheck);
+            nonBusinessInbound.Add(validation);
+            nonBusinessInbound.Add(persistence);
+        }
+
+        public Pipeline Create(UseCase useCase, Channel channel)
+        {
+            if (useCase == UseCase.Business && channel == Channel.Inbound)
+            {
+                return businessInbound;
+            }
+            return nonBusinessInbound;
+        }
+    }
+}
---
+++ AP/Receiver/ResponderFactory.cs
@@ -0,0 +1,19 @@
+﻿using AP.Receiver.Responders;
+
+namespace AP.Receiver
+{
+    public class ResponderFactory
+    {   
+        private IResponder receiptAndError = new ReceiptAndErrorResponder();
+        private IResponder errorOnly = new ErrorOnlyResponder();
+
+        public IResponder Create(UseCase useCase)
+        {
+            if (useCase == UseCase.System)
+            {
+                return receiptAndError;
+            }
+            return errorOnly;
+        }
+    }
+}
