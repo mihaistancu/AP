35b315e1f6cef21e6546818566e3f0f0f8576a1b
ubuntucsharp <ubuntucsharp@gmail.com>
2021-08-05 10:47:37 +0200

    Add workers for business message processing

---
+++ AP/Processing/Workers/ArchivingWorker.cs
@@ -0,0 +1,10 @@
+﻿namespace AP.Processing.Workers
+{
+    public class ArchivingWorker : IWorker
+    {
+        public WorkerOutput Process(WorkerInput input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ AP/Processing/Workers/ValidationWorker.cs
@@ -0,0 +1,10 @@
+﻿namespace AP.Processing.Workers
+{
+    public class ValidationWorker : IWorker
+    {
+        public WorkerOutput Process(WorkerInput input)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
