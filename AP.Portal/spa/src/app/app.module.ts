8f56cedef71dc39e4531cca97e0080f2e42c6438
ubuntucsharp <ubuntucsharp@gmail.com>
2021-11-04 16:57:48 +0100

    Add routing rules component

--- AP.Portal/spa/src/app/app.module.ts
+++ AP.Portal/spa/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoutingRulesComponent } from './routing-rules/routing-rules.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingRulesComponent
   ],
   imports: [
     BrowserModule,
---
+++ AP.Portal/spa/src/app/routing-rules/routing-rules.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-routing-rules',
+  template: `
+    <p>
+      routing-rules works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class RoutingRulesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
