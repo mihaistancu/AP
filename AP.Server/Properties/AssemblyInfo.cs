25854cc492916b923d71c71a3c8e789aedab948c
ubuntucsharp <ubuntucsharp@gmail.com>
2021-11-04 20:46:43 +0100

    Improved project names

--- AP.Server/Properties/AssemblyInfo.cs
+++ AP.Server/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AP.Server")]
+[assembly: AssemblyTitle("AP.Endpoints")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AP.Server")]
+[assembly: AssemblyProduct("AP.Endpoints")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
