b1bd65f7f325016eeb26014c0b2997ebb65598e8
ubuntucsharp <ubuntucsharp@gmail.com>
2021-11-11 16:02:11 +0100

    Create separate group component

--- AP.Portal/src/app/app.module.ts
+++ AP.Portal/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RoutingComponent } from './routing/routing.component';
+import { GroupComponent } from './routing/group/group.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RoutingComponent
+    RoutingComponent,
+    GroupComponent
   ],
   imports: [
     BrowserModule,
