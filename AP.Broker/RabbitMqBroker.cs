12e1d88d656ada2b0d80c12560c36bc852a0f624
mihai stancu <Mihai-Dorian.STANCU@ext.ec.europa.eu>
2021-12-29 15:31:01 +0100

    Pass headers to broker

--- AP.Broker/RabbitMqBroker.cs
+++ AP.Broker/RabbitMqBroker.cs
@@ -2,6 +2,7 @@
 using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace AP.Broker
@@ -27,8 +28,13 @@ namespace AP.Broker
             var consumer = new AsyncEventingBasicConsumer(receiveChannel);
             consumer.Received += (sender, args) =>
             {
+                var headers = args.BasicProperties.Headers == null
+                   ? new Dictionary<string, object>()
+                   : new Dictionary<string, object>(args.BasicProperties.Headers);
+
                 return Task.Run(() => handler.Invoke(new Command
                 {
+                    Headers = headers,
                     Payload = args.Body.ToArray()
                 }));
             };
@@ -45,10 +51,13 @@ namespace AP.Broker
         {
             using (var sendChannel = connection.CreateModel())
             {
+                var properties = sendChannel.CreateBasicProperties();
+                properties.Headers = command.Headers;
+
                 sendChannel.BasicPublish(
                     exchange: "",
                     routingKey: "hello",
-                    basicProperties: null,
+                    basicProperties: properties,
                     body: command.Payload);
             }
         }
--- AP.Orchestration/Command.cs
+++ AP.Orchestration/Command.cs
@@ -4,7 +4,7 @@ namespace AP.Orchestration
 {
     public class Command
     {
-        public Dictionary<string, string> Headers { get; set; }
+        public Dictionary<string, object> Headers { get; set; }
         public byte[] Payload { get; set; }
     }
 }
