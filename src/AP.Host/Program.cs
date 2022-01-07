6e3a3834878069f1ea42b188aa2d2b9eb85f799c
mihai stancu <Mihai-Dorian.STANCU@ext.ec.europa.eu>
2022-01-07 17:58:40 +0100

    Display messages continuously for easy testing

--- src/AP.Host/Program.cs
+++ src/AP.Host/Program.cs
@@ -1,2 +1,14 @@
-﻿Console.WriteLine("Press [enter] to stop");
-Console.ReadLine();
+﻿var x = new Utils();
+
+while (true)
+{
+    x.Write();
+    System.Threading.Thread.Sleep(1000);
+}
+
+public class Utils{
+    public void Write()
+    {
+        Console.WriteLine("hello");
+    }
+}
