5c03efb976d5582f28633a39f2fbc9fc5e82782d
mihai <stancu.mihai.dorian@gmail.com>
2022-01-13 15:43:46 +0000

    Moved all cs files

--- backend/src/AP.Host/Program.cs
+++ backend/src/AP.Host/Program.cs
@@ -1,14 +1,53 @@
-﻿var x = new Utils();
+﻿using AP.Dependencies;
+using System;
 
-while (true)
+namespace AP.Host
 {
-    x.Write();
-    System.Threading.Thread.Sleep(1000);
-}
-
-public class Utils{
-    public void Write()
+    class Program
     {
-        Console.WriteLine("hello");
+        static void Main(string[] args)
+        {
+            Context.Build();
+
+            using (StartMetrics())
+            using (StartTrace())
+            using (StartOrchestrator())
+            using (StartMessagingServer())
+            using (StartPortalServer())
+            {
+                Console.WriteLine("Press [enter] to stop");
+                Console.ReadLine();
+            }
+        }
+
+        public static IDisposable StartMetrics()
+        {
+            return Context.Metrics.Start();
+        }
+
+        public static IDisposable StartTrace()
+        {
+            return Context.Trace.Start();
+        }
+
+        private static IDisposable StartOrchestrator()
+        {
+            return Context.Orchestrator.Start();
+        }
+
+        private static IDisposable StartMessagingServer()
+        {
+            var messaging = Context.ServerFactory.Create();
+            Context.MessageEndpoints.Apply(messaging);
+            return messaging.Start("http://localhost:9000");
+        }
+
+        private static IDisposable StartPortalServer()
+        {
+            var portal = Context.ServerFactory.Create();
+            Context.PortalApi.Apply(portal);
+            Context.PortalSpa.Apply(portal);
+            return portal.Start("http://localhost:9090");
+        }
     }
 }
